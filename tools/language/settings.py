ba580d6955f00c36bf7d8b95cc61c3d42916be29
ndgigliotti <ndgigliotti@gmail.com>
2021-09-26 14:07:18 -0400

    initial copy

---
+++ tools/language/settings.py
@@ -0,0 +1,10 @@
+import re
+
+DEFAULT_TOKENIZER = re.compile(r"\b\w\w+\b").findall
+"""Default tokenizer to use."""
+
+DEFAULT_SEP = "_"
+"""Default separator to use for tagging or joining words."""
+
+CACHE_SIZE = int(1_000_000)
+"""Maximum number of recent calls to keep for functions with LRU caching."""
---
+++ tools/sklearn/__init__.py
@@ -0,0 +1,2 @@
+"""Tools related to Scikit-Learn API.."""
+from . import selection, vectorizers
